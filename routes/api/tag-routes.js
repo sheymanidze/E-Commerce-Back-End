1015183f481e870be6840362fc7ddd8835dc5667
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-20 14:56:31 -0400

    create for tag added

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,6 +33,12 @@ router.get('/:id', async (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  try {
+    const tagInfo = await Tag.create(req.body);
+    res.status(200).json(tagInfo);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
