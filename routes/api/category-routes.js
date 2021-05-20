ad74b183254258f895564301badb86daa75ffd7c
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-20 10:47:36 -0400

    get / for all routes added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,9 +3,17 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categoryInfo = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
