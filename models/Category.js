fe04ec5feebb277f36d8d619c566447691571de6
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-20 10:22:10 -0400

    category model added

--- models/Category.js
+++ models/Category.js
@@ -2,11 +2,25 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+
+    //id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    //category_name column
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
