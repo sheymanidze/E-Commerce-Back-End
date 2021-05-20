e259ce8066b76542089f07690310d49b2a6a0145
Anastasia Sorkin <sorkin.anastasia@gmail.com>
2021-05-20 10:12:30 -0400

    ProductTag model added

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,12 +2,37 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    //id columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    //tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
+
+
   {
     sequelize,
     timestamps: false,
